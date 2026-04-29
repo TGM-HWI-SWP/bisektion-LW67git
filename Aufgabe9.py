64a38b2c1e9d5784d373f32dc14619b9a541cd10
LW67git <lwendt@student.tgm.ac.at>
2026-04-29 19:45:33 +0200

    Aufgabe 9 fertig

--- Aufgabe9.py
+++ Aufgabe9.py
@@ -1,8 +1,48 @@
 from Aufgabe5 import solver
 from Aufgabe6 import solver2
-from Aufgabe7 import plotter
+import math
+
+
+def solver_aufgabe9() -> None:
+    """Berechnet den Krümmungsradius einer Kettenlinie und die Länge des Seils, mithilfe vom Bisektionsverfahren und Regula Falsi-Verfahren (Überprüfung)
+    
+    Returns: None
+    """
+
+
+    print("-" * 50)
+    print("Mit solver (Bisektionsverfahren):")
+    funktion = "x * math.cosh(50/x) - x -10"
+    
+
+    a = 50
+    b = 150
+    epsilon = 0.00001
+
+    radius, iterationen = solver(funktion, a, b, epsilon)
+
+    w = 100
+
+    länge = 2 * radius * math.sinh(w/(2*radius))
+
+    print(f"Radius: {radius}")
+    print(f"Anzahl der Iterationen: {iterationen}")
+    print(f"Länge des Seils: {länge}")
+    print("-" * 50)
+    print("-" * 50)
+    print("-" * 50)
+
+    print("Mit solver2 (Regula Falsi):")
+    
+    radius, iterationen = solver2(funktion, a, b, epsilon)
+
+    länge = 2 * radius * math.sinh(w/(2*radius))
+
+    print(f"Radius: {radius}")
+    print(f"Anzahl der Iterationen: {iterationen}")
+    print(f"Länge des Seils: {länge}")
+    print("-" * 50)
+
 
 if __name__ == "__main__":
-    solver()
-    solver2()
-    plotter()
+    solver_aufgabe9()
